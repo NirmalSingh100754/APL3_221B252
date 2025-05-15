36dad8ac790b8c794c3cd98735bfa977464938ff
Nirmal Singh <nirmalsingh100754@hotmail.com>
2025-05-15 11:57:15 +0530

    Lab Exercise 6

---
+++ LE6/Coffee.java
@@ -0,0 +1,10 @@
+
+class Coffee extends Offering{
+    int getPrice(){
+        return 55;
+    }
+    
+    String getName(){
+        return "Coffee";
+    }
+}
---
+++ LE6/Icecream.java
@@ -0,0 +1,9 @@
+class Icecream extends Offering{
+    int getPrice(){
+        return 35;
+    }
+    
+    String getName(){
+        return "Icecream";
+    }
+}
